5ec0a1893f4c906e3eb7f6a076882af03ec385b8
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-12-09 17:49:21 +0000

    refactor: Use rich instead of prettytable for displaying results

--- advent_of_code_2023/utils.py
+++ advent_of_code_2023/utils.py
@@ -1,47 +1,47 @@
-from enum import Enum
-from pathlib import Path
 from functools import wraps
+from pathlib import Path
 from time import perf_counter
-from typing import TypeVar, ParamSpec, Callable
+from typing import Callable, ParamSpec, TypeVar
 
-from prettytable import PrettyTable, SINGLE_BORDER
+from advent_of_code_2023.console import console
+from rich.box import ROUNDED
+from rich.table import Table
 
 T = TypeVar("T")
 P = ParamSpec("P")
 
 
-def get_input_as_lines(
-    input_path: Path,
-    encoding: str = "utf-8",
-) -> list[str]:
-    with open(input_path, "r", encoding=encoding) as f:
-        return f.readlines()
+def get_input_as_lines(input_path: Path, encoding: str = "utf-8") -> list[str]:
+	with open(input_path, "r", encoding=encoding) as f:
+		return f.readlines()
 
 
-def get_input_as_content(
-    input_path: Path,
-    encoding: str = "utf-8",
-) -> str:
-    with open(input_path, "r", encoding=encoding) as f:
-        return f.read()
+def get_input_as_content(input_path: Path, encoding: str = "utf-8") -> str:
+	with open(input_path, "r", encoding=encoding) as f:
+		return f.read()
 
 
-def pretty_output(title: str):
-    def decorator(func: Callable[P, T]) -> Callable[P, T]:
-        @wraps(func)
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
-            start_time = perf_counter()
-            result = func(*args, **kwargs)
-            elapsed_time = perf_counter() - start_time
+def pretty_output(title: str, caption: str):
+	def decorator(func: Callable[P, T]) -> Callable[P, T]:
+		@wraps(func)
+		def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+			start_time = perf_counter()
+			result = func(*args, **kwargs)
+			elapsed_time = perf_counter() - start_time
 
-            table = PrettyTable(title=title, encoding="utf-8", align="l", header=False)
-            table.set_style(SINGLE_BORDER)
-            table.add_rows([
-                (f"Answer: {result}",),
-                (f"Time: {elapsed_time:.6f} seconds",)
-            ])
-            print(table)
+			table = Table(title=title, caption=caption, box=ROUNDED, safe_box=True)
+			table.add_column(
+				"Answer", style="cyan", justify="center", vertical="middle"
+			)
+			table.add_column(
+				"Time", style="magenta", justify="center", vertical="middle"
+			)
+			table.add_row(f"{result}", f"{elapsed_time:.6f} seconds")
 
-            return result
-        return wrapper
-    return decorator
+			console.print(table, new_line_start=True)
+
+			return result
+
+		return wrapper
+
+	return decorator
