f519f7ccb97ba528027980d34edc89544f3890c7
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-12-09 17:48:33 +0000

    feat(day-9): Day 9 solution part 1 and 2

---
+++ advent_of_code_2023/day_9/part_2.py
@@ -0,0 +1,30 @@
+from itertools import pairwise
+from pathlib import Path
+
+from advent_of_code_2023.utils import get_input_as_lines, pretty_output
+
+
+def extrapolate(sequence: list[int]) -> int:
+	if all(x == 0 for x in sequence):
+		return 0
+
+	deltas = [b - a for a, b in pairwise(sequence)]
+	return sequence[0] - extrapolate(deltas)
+
+
+@pretty_output(title="Day 9: Mirage Maintenance", caption="part - 2")
+def solution(lines: list[str]) -> int:
+	totals = 0
+
+	for line in lines:
+		totals += extrapolate(list(map(int, line.split())))
+
+	return totals
+
+
+def main() -> None:
+	solution(get_input_as_lines(Path("./input.txt")))
+
+
+if __name__ == "__main__":
+	main()
